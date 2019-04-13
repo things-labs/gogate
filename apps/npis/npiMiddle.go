cf8d4943bbe76238c6246f3abf59691120d182bb
mo <thinkgo@aliyun.com>
2019-04-13 20:16:00 +0800

    使用uber的依赖注入方式

--- apps/npis/npiMiddle.go
+++ apps/npis/npiMiddle.go
@@ -13,6 +13,26 @@ type MiddleMonitor struct {
 	IncommingMsgPkt chan *ltl.IncomingMsgPkt
 }
 
+func NewMiddleMonitor(m *npi.Monitor) *MiddleMonitor {
+	mid := &MiddleMonitor{
+		IncommingMsgPkt: make(chan *ltl.IncomingMsgPkt, Incomming_msg_size_max),
+		Monitor:         m,
+	}
+
+	mid.AddAsyncCbs(map[uint16]func(*npi.Npdu){
+		npi.MT_AF_DATA_CONFIRM:                        Af_DataConfirm,
+		npi.MT_AF_INCOMING_MSG:                        Af_IncomingMsg,
+		npi.MT_ZDO_MGMT_PERMIT_JOIN_RSP:               Zdo_MgmtPermitJoinRsp,
+		npi.MT_ZDO_STATE_CHANGE_IND:                   Zdo_StateChangeInd,
+		npi.MT_ZDO_END_DEVICE_ANNCE_IND:               Zdo_EnddeviceAnnceInd,
+		npi.MT_ZDO_LEAVE_IND:                          Zdo_LeaveInd,
+		npi.MT_SYS_RESET_IND:                          Sys_ResetInd,
+		npi.MT_APP_CNF_BDB_COMMISSIONING_NOTIFICATION: Appcfg_BdbCommissioningNotice,
+	})
+
+	return mid
+}
+
 func (this *MiddleMonitor) writeIncommingMsg(msg *ltl.IncomingMsgPkt) {
 	select {
 	case <-this.CloseChan():
--- apps/npis/zbApp.go
+++ apps/npis/zbApp.go
@@ -10,6 +10,7 @@ import (
 
 	"github.com/astaxie/beego/logs"
 	"github.com/tarm/serial"
+	"go.uber.org/dig"
 )
 
 const Incomming_msg_size_max = 256
@@ -23,15 +24,13 @@ type ZbnpiApp struct {
 
 var ZbApps *ZbnpiApp
 
-func ZbAppInit() error {
-	var m *npi.Monitor
-
+func NewSerialConfig() (*serial.Config, error) {
 	bcfg := misc.UartCfg
 	usartcfg := &serial.Config{}
 
 	secCom0, err := bcfg.GetSection("COM0")
 	if err != nil {
-		return err
+		return nil, err
 	}
 
 	usartcfg.Name = secCom0.Key("Name").MustString("COM0")
@@ -39,39 +38,31 @@ func ZbAppInit() error {
 	usartcfg.Size = byte(secCom0.Key("DataBit").MustUint(8))
 	usartcfg.Parity = serial.Parity(secCom0.Key("Parity").MustInt('N'))
 	usartcfg.StopBits = serial.StopBits(secCom0.Key("StopBit").MustInt(1))
-
 	logs.Debug("usarcfg: %#v", usartcfg)
 
-	if m, err = npi.NewNpiMonitor(usartcfg); err != nil {
-		logs.Error("npi new failed", err)
-		return err
-	}
+	return usartcfg, nil
+}
 
-	mid := &MiddleMonitor{
-		IncommingMsgPkt: make(chan *ltl.IncomingMsgPkt, Incomming_msg_size_max),
-		Monitor:         m,
-	}
-
-	mid.AddAsyncCbs(map[uint16]func(*npi.Npdu){
-		npi.MT_AF_DATA_CONFIRM:                        Af_DataConfirm,
-		npi.MT_AF_INCOMING_MSG:                        Af_IncomingMsg,
-		npi.MT_ZDO_MGMT_PERMIT_JOIN_RSP:               Zdo_MgmtPermitJoinRsp,
-		npi.MT_ZDO_STATE_CHANGE_IND:                   Zdo_StateChangeInd,
-		npi.MT_ZDO_END_DEVICE_ANNCE_IND:               Zdo_EnddeviceAnnceInd,
-		npi.MT_ZDO_LEAVE_IND:                          Zdo_LeaveInd,
-		npi.MT_SYS_RESET_IND:                          Sys_ResetInd,
-		npi.MT_APP_CNF_BDB_COMMISSIONING_NOTIFICATION: Appcfg_BdbCommissioningNotice,
+func ZbAppInit() error {
+	container := dig.New()
+	container.Provide(NewSerialConfig)
+	container.Provide(npi.NewNpiMonitor)
+	container.Provide(NewMiddleMonitor)
+	container.Provide(func(mid *MiddleMonitor) *ZbnpiApp {
+		return &ZbnpiApp{
+			Ltl_t:         ltl.NewClient(mid),
+			MiddleMonitor: mid,
+		}
 	})
 
-	ZbApps = &ZbnpiApp{
-		Ltl_t:         ltl.NewClient(mid),
-		MiddleMonitor: mid,
-	}
+	err := container.Invoke(func(app *ZbnpiApp) {
+		go app.ServerInApdu(app)
+		app.MiddleMonitor.Start()
+		app.NetworkFormation()
+		ZbApps = app
+	})
 
-	go ZbApps.ServerInApdu(ZbApps)
-	ZbApps.MiddleMonitor.Start()
-
-	return ZbApps.NetworkFormation()
+	return err
 }
 
 // 建立zigbee的网络
